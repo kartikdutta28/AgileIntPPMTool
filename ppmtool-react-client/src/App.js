e02856e3da6c4e765f3a78721abbc1a3ffe8d21d
KartikeDutta <kartikdutta29@gmail.com>
2019-09-12 21:31:57 +0530

    Design Tool Added

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
-import ProjectItem from "./components/project/ProjectItem";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
