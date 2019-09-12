51e70fa8190a151b8a3470b84c9904ab180428e5
KartikeDutta <kartikdutta29@gmail.com>
2019-09-12 20:57:48 +0530

    Usual commit

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,25 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
+import ProjectItem from "./components/project/ProjectItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>PPM Tool</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Dashboard />
     </div>
   );
 }
