50c57497713e580080cdf145d7f8bdb0cacefde5
KartikeDutta <kartikdutta29@gmail.com>
2019-09-20 22:09:36 +0530

    branch75

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
-import Header from "./components/layout/Header";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
@@ -11,6 +11,9 @@ import UpdateProject from "./components/project/UpdateProject";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
 class App extends Component {
   render() {
     return (
@@ -18,6 +21,17 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+            {
+              //Public Routes
+            }
+
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+            {
+              //Private Routes
+            }
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
