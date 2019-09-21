47828a54f07dcb833a5c7de853a7557247bf3b8a
KartikeDutta <kartikdutta29@gmail.com>
2019-09-21 11:05:20 +0530

    Security Frontend completed

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -2,16 +2,29 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
+const initalState = {};
 const middleware = [thunk];
+
 let store;
-store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(
+      applyMiddleware(...middleware),
+      ReactReduxDevTools
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 export default store;
