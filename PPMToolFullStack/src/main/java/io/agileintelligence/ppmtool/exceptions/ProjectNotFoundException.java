c4197a275b10c0c2004ff73b7a59655f1bdfa696
KartikeDutta <kartikdutta29@gmail.com>
2019-10-15 11:14:15 +0530

    Updated jwt sesssion time

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/exceptions/ProjectNotFoundException.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/exceptions/ProjectNotFoundException.java
@@ -1,4 +1,5 @@
 package io.agileintelligence.ppmtool.exceptions;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,5 +1,4 @@
 package io.agileintelligence.ppmtool.web;
-
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.ProjectService;
@@ -7,14 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/project")
 @CrossOrigin
